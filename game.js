ee11b99755e1e7b685ef29ac1f7df33c7e718293
octuspi <octuspi@gmail.com>
2024-07-08 02:16:44 -0300

    version 2.0

--- game.js
+++ game.js
@@ -1,97 +1,98 @@
-let state = true
-const board = [...document.querySelectorAll('.column')]
+const celulas = document.querySelectorAll('.col')
+const btreset = document.getElementById('btn_reset')
+const scoreX  = document.getElementById('scorex')
+const scoreO  = document.getElementById('scoreo')
 
-function mark(players, board){
-    if(board){
-        let index = 0
-
-        board.forEach((column, i) => {
-            column.addEventListener('click', e => {
-                if(e.target.getAttribute('data-mark') === '' && state){
-                    e.target.innerHTML = players[index].id
-                    e.target.setAttribute('data-mark', players[index].id)
-
-                    if(iswin(players[index], board, i)){
-                        state = false
-                        updatescore(players[index])
-                    }
-
-                    if(players.length - 1 > index){
-                        index++
-                    }else{
-                        index = 0
-                    }
-                }
-            })
-        });
+const game = {
+    start: true,
+    cels_winner: [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]],
+    player1:{
+        score: 0,
+        turn: false,
+        winner: false,
+        mark: 'X',
+        marked: [],
+    },
+    player2:{
+        score: 0,
+        turn: false,
+        winner: false,
+        mark: 'O',
+        marked: [],
     }
 }
 
-function iswin(player, board, markedcell){
-    const win = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
-    let winner = false
+function player_time(p1, p2){
+    if(p1.marked.length == 0 && p2.marked.length == 0){
+        return p2.winner ? p2 : p1
+    }
 
-    win.forEach(w => {
-        if(w.includes(markedcell)){
-            let group = []
-            w.forEach(c => {
-                let getMark = board[c].getAttribute('data-mark')
-                group.push({
-                    cell:board[c],
-                    check:getMark === player.id
-                })
-            })
+    if(p1.turn){
+        p1.turn = false
+        return p2
+    }else{
+        p2.turn = false
+        return p1
+    }
+}
 
-            if(group.every(current => current.check === true)){
-                player.score++
-                winner = true
-
-                group.forEach(obj => {
-                    obj.cell.classList.add('markedcell')
-                });
-
-                return
+function check_win(player){
+    if(player.marked.length >= 3){
+        game.cels_winner.forEach(c => {
+            function ok(id){
+                return player.marked.includes(id)
             }
-        }
-    })
-
-    return winner
-}
-
-function updatescore(player){
-    const score = document.getElementById(player.id === 'X' ? 'score-x' : 'score-o')
-    if(score){
-        score.innerHTML = player.score
-    }
-}
-
-function startgame(){
-    const player1 = {
-        id: 'X',
-        score: 0
-    }
-
-    const player2 = {
-        id: 'O',
-        score: 0
-    }
-
-    mark([player1, player2], board)
-}
-
-function resetgame(){
-    const reset = document.getElementById('reset')
-    if(reset){
-        reset.addEventListener('click', e => {
-            state = true
-            board.forEach(column => {
-                column.setAttribute('data-mark', '')
-                column.classList.remove('markedcell')
-                column.innerHTML = ''
-            })
+        
+            if(c.every(ok)){
+                game.start = false
+                update_score(player)
+                c.forEach(i => {
+                    celulas[i].classList.add('winner')
+                })
+                return true
+            }
         })
     }
 }
 
-startgame()
-resetgame()
+function update_score(player){
+    player.score++
+    
+    scoreX.innerText = `Player ${game.player1.mark} : ${game.player1.score}`
+    scoreO.innerText = `Player ${game.player2.mark} : ${game.player2.score}`
+    game.player1.winner = false
+    game.player2.winner = false
+    
+    player.winner = true
+}
+
+function reset_game(){
+    game.start = true
+    game.player1.marked = []
+    game.player2.marked = []
+
+    celulas.forEach(cel => {
+        cel.innerText = ''
+        cel.classList.remove('winner')
+    })
+}
+
+function start_game(){
+    celulas.forEach((cel, index) => {
+        
+        cel.addEventListener('click', () => {
+            if(game.start && cel.innerText == ''){
+                const player = player_time(game.player1, game.player2)
+                cel.innerText = player.mark
+                player.marked.push(index)
+                player.turn = true
+                check_win(player)
+            }
+        })
+    })
+
+    btreset.addEventListener('click', reset_game)
+
+}
+
+start_game()
